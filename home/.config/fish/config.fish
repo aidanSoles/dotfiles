f3b30372b704598cea844bde05dac9036c9a9cd0
aidanSoles <aidan.soles@gmail.com>
2021-12-25 14:42:15 -0600

    add vim bindings

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -9,3 +9,9 @@ fish_add_path ~/.local/bin
 source ~/.config/fish/conf.d/aliases.fish
 
 # end: aliases
+
+# start: key bindings
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+# end: key bindings
