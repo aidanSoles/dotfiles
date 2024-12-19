092dae54ed58dd2ba3254393e328abd8798cb98e
Aidan Daniels-Soles <aidan.soles@gmail.com>
2024-12-19 09:51:12 -0800

    set coq.nvim ctrl + h option to a noop

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -154,6 +154,9 @@ require("lazy").setup({
 			init = function()
 				vim.g.coq_settings = {
 					auto_start = "shut-up",
+					keymap = {
+						jump_to_mark = ""
+					}
 				}
 			end,
 			config = function()
